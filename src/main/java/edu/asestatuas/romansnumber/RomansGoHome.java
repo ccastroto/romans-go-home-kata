83505a27783c492bb0a3e3d483b33be9e604f262
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-05-15 14:09:34 +0200

    refactor(romannumber): refacorizar el cod para compaginar los dos main

--- src/main/java/edu/asestatuas/romansnumber/RomansGoHome.java
+++ src/main/java/edu/asestatuas/romansnumber/RomansGoHome.java
@@ -3,25 +3,19 @@ package edu.asestatuas.romansnumber;
 import java.util.List;
 import  edu.asestatuas.romansnumber.RomanNumberSymbol;
 
-/*public class RomansGoHome {
+public class RomansGoHome {
 
     public static void main(String[] args) {
 
-        List.of( "MMMDCCCLXXXVIII", // 3888
+        /*List.of( "MMMDCCCLXXXVIII", // 3888
                         "MMDCCLXXVII",  // 2777
                         "CDXLIV", // 444
                         "CDXXXIX" // 439
                 ).stream()
                 .map(RomanNumber::new)
-                .forEach(n -> System.out.println(n.toString() + " = " + n.toDecimal()));
-    }
-}*/
+                .forEach(n -> System.out.println(n.toString() + " = " + n.toDecimal()));*/
 
-public class RomansGoHome {
-
-    public static void main(String[] args) {
         RomanToDecimal deci = new RomanToDecimal("MMCDXLV");
         deci.Breaker();
     }
-
 }
