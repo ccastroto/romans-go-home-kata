6fd29ea03399f51e0ba82f60f9175ce16457f4aa
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-05-15 13:57:28 +0200

    refactor(romannumber): crear un main de prueba

--- src/main/java/edu/asestatuas/romansnumber/RomansGoHome.java
+++ src/main/java/edu/asestatuas/romansnumber/RomansGoHome.java
@@ -1,8 +1,9 @@
 package edu.asestatuas.romansnumber;
 
 import java.util.List;
+import  edu.asestatuas.romansnumber.RomanNumberSymbol;
 
-public class RomansGoHome {
+/*public class RomansGoHome {
 
     public static void main(String[] args) {
 
@@ -14,4 +15,13 @@ public class RomansGoHome {
                 .map(RomanNumber::new)
                 .forEach(n -> System.out.println(n.toString() + " = " + n.toDecimal()));
     }
+}*/
+
+public class RomansGoHome {
+
+    public static void main(String[] args) {
+        RomanToDecimal deci = new RomanToDecimal("MMCDXLV");
+        deci.Breaker();
+    }
+
 }
