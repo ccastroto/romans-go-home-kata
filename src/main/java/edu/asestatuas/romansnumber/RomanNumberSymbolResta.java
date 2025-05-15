5e04e1a05e0626eead724c750093b05815cd0c01
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-05-15 13:56:54 +0200

    feat(romansymbolresta): definir los numeros romanos que resta

---
+++ src/main/java/edu/asestatuas/romansnumber/RomanNumberSymbolResta.java
@@ -0,0 +1,18 @@
+package edu.asestatuas.romansnumber;
+
+public enum RomanNumberSymbolResta {
+
+    IV(4), IX(9), XL(40), XC(90), CD(400), CM(900);
+
+    public int numero;
+
+    RomanNumberSymbolResta(int numero) {
+        this.numero = numero;
+    }
+
+    public  int getNumber() {
+        return this.numero;
+    }
+
+
+}
