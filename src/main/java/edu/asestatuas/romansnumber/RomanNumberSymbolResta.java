54578ace9cd18336e19a1ce12f98a74a634b58e8
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-05-19 12:20:54 +0200

    refactor(romansymbolresta): refatorizar a patron singleton

--- src/main/java/edu/asestatuas/romansnumber/RomanNumberSymbolResta.java
+++ src/main/java/edu/asestatuas/romansnumber/RomanNumberSymbolResta.java
@@ -4,7 +4,7 @@ public enum RomanNumberSymbolResta {
 
     IV(4), IX(9), XL(40), XC(90), CD(400), CM(900);
 
-    public int numero;
+    private int numero;
 
     RomanNumberSymbolResta(int numero) {
         this.numero = numero;
